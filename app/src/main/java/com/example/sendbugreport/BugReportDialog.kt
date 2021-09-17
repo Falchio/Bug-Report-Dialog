f417b10e9d93503efbb00b963789252dc9f7f6e8
ProkhorovKV <ProkhorovKV@Piteravto.ru>
2021-09-17 20:36:14 +0300

    Сделан спиннер.

--- app/src/main/java/com/example/sendbugreport/BugReportDialog.kt
+++ app/src/main/java/com/example/sendbugreport/BugReportDialog.kt
@@ -3,11 +3,20 @@ package com.example.sendbugreport
 import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
-class BugReportDialog(context: Context): Dialog(context) {
+class BugReportDialog(context: Context) : Dialog(context) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.bug_report_dialog)
+        val spinner: Spinner = findViewById(R.id.spinner)
+        val list = listOf("Сейчас", "Новостная лента")
+        val adapter =
+            ArrayAdapter(this.context,  android.R.layout.simple_spinner_item, list)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
 
     }
 }
